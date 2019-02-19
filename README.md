14852af842840e9f22a18758336ca43b1c524cae
fkafri <firas.alkafri@gmail.com>
2019-02-19 10:55:15 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,194 @@
+# Django Propaganda
+
+Django Propaganda is a simple pub/sub utility for [Kombu]. 
+
+Define your broker's URL in your django settings:
+
+```
+PROPAGANDA_BROKER_URL = "amqp://guest:guest@localhost:5672//"
+```
+
+Give Propaganda a topic exchange name and it provides a nice interface for pub/sub. 
+
+## Installation
+
+```
+pip install django-propaganda
+```
+
+## Creating Propaganda Object
+
+```python
+from django_propaganda import Propaganda
+
+propaganda = Propaganda("exchange_topic_name")
+```
+
+## Publishing
+
+Publish method takes routing key and payload parameters:
+
+```python
+propaganda.publish('my.routing.key', {'hello': 'world'})
+```
+
+You can omit the payload which defaults to an empty dictionary.
+
+```python
+propaganda.publish('my.routing.key')
+```
+
+`publish` method by default blocks the execution until message is sent. You can add `block=False` parameter to make 
+publish return immediately, and message will be sent in a background thread.
+
+```python
+# this will return after creating a thread that will send the message
+propaganda.publish('my.routing.key', {'hello': 'world'}, block=False)  
+```
+
+#### Make Persistent Additions to Payloads
+
+Propaganda object has a dictionary property named `payload`. Content of `payload` will be added
+to the payload given to the publish method.
+
+```python
+propaganda.payload['sender'] = 'Yigit Ozen'
+
+# this will publish {'sender': 'Yigit Ozen', 'hello': 'world'}
+propaganda.publish('my.routing.key', {'hello': 'world'})
+
+# sender will still be added if you omit the payload parameter. 
+# this will publish {'sender': 'Yigit Ozen'}
+propaganda.publish({'my.routing.key')
+
+# use del to stop adding a key-value pair to the payloads
+del propaganda.payload['sender']
+```
+
+## Subscribing
+
+Propaganda's `subscribe` method creates and returns a Subscription object. Subscription creates a 
+queue and starts consuming it on a separate thread. 
+
+`on` method registers a callback for a certain routing key.
+ 
+`all` method registers a callback for all routing keys.
+
+The signature of the callbacks must take two arguments: (body, message), which is the decoded message body and Kombu Message instance.
+
+`wait` method blocks until a message arrives with the given routing key.
+
+`wait_any` method blocks until any message arrives.
+
+```python
+def on_rabbit(body, message):
+    print('Look, a rabbit!')
+    print(body)
+    
+sub = propaganda.subscribe('animals.#')
+
+# from now on when a animals.rabbit message arrives, on_rabbit will be called
+sub.on('animals.rabbit', on_rabbit)
+
+# when any message matching animals.# arrives, on_animal will be called
+sub.all(on_animal)
+
+# this will block the program until an animals.rabbit message arrives
+sub.wait('animals.rabbit')
+
+# wait has the same semantic with Python's multithreading wait. so you can pass a timeout.
+# return value will be true if a message arrives before the timeout, false otherwise
+arrived = sub.wait('animals.rabbit', timeout=10)
+
+# blocks until any message arrives
+sub.wait_any()
+```
+
+`on` and `all` methods return the Subscription object, so what you can chain the calls.
+
+```python
+sub = propaganda.subscribe('animals.#')
+            .on('animals.rabbit', on_rabbit)
+            .on('animals.turtle', on_turtle)
+            .all(on_animal)
+```
+
+#### Exception Handling
+
+You can pass an exception handler to `on` and `all` methods. Propaganda will call your handler with the exception 
+when an exception is raised from on of your callbacks instead of raising it.  
+For example you can use this to register loggers.
+
+```python
+def log_mq_exception(exception):
+    logger.error('Exception occurred when handling MQ message: {0}'.format(exception))
+    
+sub = propaganda.subscribe('animals.#')
+
+sub.on('animals.rabbit', on_rabbit, on_exception=log_mq_exception)
+sub.all(on_animal, on_exception=log_mq_exception) 
+```
+
+You can also pass an exception handler to `sub` method in the same way. 
+It applies to all callbacks under that subscription. 
+It is called **after** the handler given to `on` or `all` method.
+
+The following code works exactly like the one above.
+
+```python
+def log_mq_exception(exception):
+    logger.error('Exception occurred when handling MQ message: {0}'.format(exception))
+    
+sub = propaganda.subscribe('animals.#', on_exception=log_mq_exception)
+
+sub.on('animals.rabbit', on_rabbit)
+sub.all(on_animal) 
+```
+
+## Stop and Restart Consuming
+
+Subscription creates a queue and starts a thread that consumes it upon creation.
+You can destroy the queue and the thread without destroying the Subscription, and you can recreate them later.
+
+```python
+# starts listening for the events
+sub = propaganda.subscribe('animals.#')
+            .on('animals.rabbit', on_rabbit)
+            .on('animals.turtle', on_turtle)
+
+# you don't want to receive messages for a while
+sub.stop()
+
+# the registered callbacks are still there.
+# you can recreate the queue and start receiving messages again.
+sub.start()
+```
+
+If you don't keep the subscription in a variable, you can use `unsubscribe` method of Propaganda object by binding key 
+to stop consuming.
+
+```python
+propaganda.unsubscribe('animals.#')
+```
+
+## Prefixing Routing Keys
+
+You can pass a key prefix when initializing Propaganda object, which will be automatically prefixed all routing keys 
+when subscribing and publishing. If you use Propaganda object to pub/sub in a specific namespace, this will save you 
+from adding the prefix manually for all pub/sub calls.
+
+```python
+propaganda = Propaganda(connection, exchange, key_prefix='my.namespace.')
+
+# this will actually subscribe to 'my.namespace.animals.#'
+sub = propaganda.subscribe('animals.#')
+
+# on_rabbit will be called for messages with the key 'my.namespace.animals.rabbit'
+sub.on('animals.rabbit', on_rabbit)
+
+# this will publish the message with the key 'my.namespace.my.routing.key'
+propaganda.publish({'my.routing.key', 'hello': 'world'})
+```
+
+
+[Kombu]: https://github.com/celery/kombu
---
+++ django_propaganda/propaganda.py
@@ -0,0 +1,231 @@
+import logging
+from inspect import isroutine
+from socket import timeout
+from collections import defaultdict
+from time import sleep
+from threading import Thread, Event
+
+from django.conf import settings
+from kombu import Queue, Consumer, Connection, Exchange
+from kombu.pools import producers, connections
+from kombu.exceptions import MessageStateError
+
+
+logger = logging.getLogger(__name__)
+
+
+class Handler(object):
+    def __init__(self, callback, on_exception=None):
+        self._callback = callback
+
+        if on_exception is not None:
+            if not isroutine(on_exception):
+                raise ValueError('on_exception must be a function or method')
+            self._on_exception = on_exception
+        else:
+            self._on_exception = self._default_on_exception
+
+    def __call__(self, *args, **kwargs):
+        try:
+            self._callback(*args, **kwargs)
+        except Exception as e:
+            self._on_exception(e)
+
+    def _default_on_exception(self, exception):
+        raise exception
+
+
+class Subscription(object):
+    def __init__(self, connection, exchange, binding_key, key_prefix='', reconnect_timeout=10, on_exception=None):
+        self._connection = connection
+        self._exchange = exchange
+        self._binding_key = binding_key
+        self._key_prefix = key_prefix
+        self._reconnect_timeout = reconnect_timeout
+
+        if on_exception is not None:
+            if not isroutine(on_exception):
+                raise ValueError('on_exception must be a function or method')
+            self._on_exception = on_exception
+        else:
+            self._on_exception = self._default_on_exception
+
+        self._handlers = defaultdict(list)
+        self._handlers_for_all = []
+        self._events = {}
+        self._event_any = Event()
+        self._running = False
+        self._thread = None
+
+        self.start()
+
+    def __del__(self):
+        self._running = False
+        try:
+            self._thread.join()
+        except Exception as e:
+            pass
+
+    def _default_on_exception(self, exception):
+        raise exception
+
+    @property
+    def is_alive(self):
+        return self._thread is not None and self._thread.is_alive()
+
+    def start(self):
+        if not self._running:
+            self._running = True
+            self._thread = Thread(target=self._consume)
+            self._thread.start()
+
+    def stop(self):
+        self._running = False
+
+    def on(self, routing_key, callback, on_exception=None):
+        # register a callback for the given routing key
+        routing_key = '{0}{1}'.format(self._key_prefix, routing_key)
+        self._handlers[routing_key].append(Handler(callback, on_exception))
+        return self
+
+    def all(self, callback, on_exception=None):
+        # register a callback for all routing keys
+        self._handlers_for_all.append(Handler(callback, on_exception))
+        return self
+
+    def wait(self, routing_key, timeout=None):
+        # register an event wait
+        routing_key = '{0}{1}'.format(self._key_prefix, routing_key)
+        if routing_key not in self._events:
+            self._events[routing_key] = Event()
+
+        return self._events[routing_key].wait(timeout)
+
+    def wait_any(self, timeout=None):
+        return self._event_any.wait(timeout)
+
+    def _on_message(self, body, message):
+        if not self._running:
+            return
+
+        routing_key = message.delivery_info['routing_key']
+
+        if routing_key in self._events:
+            self._events[routing_key].set()
+            self._events[routing_key].clear()
+
+        self._event_any.set()
+        self._event_any.clear()
+
+        for handler in self._handlers[routing_key]:
+            try:
+                handler(body, message)
+            except Exception as e:
+                self._on_exception(e)
+
+        for handler in self._handlers_for_all:
+            try:
+                handler(body, message)
+            except Exception as e:
+                self._on_exception(e)
+
+        try:
+            message.ack()
+        except MessageStateError:
+            pass
+
+    def _consume(self):
+        routing_key = '{0}{1}'.format(self._key_prefix, self._binding_key)
+
+        while self._running:
+            try:
+                with connections[self._connection].acquire(block=True) as conn:
+                    queue = Queue(exchange=self._exchange, routing_key=routing_key, channel=conn,
+                                  durable=False, exclusive=True, auto_delete=True)
+
+                    with Consumer(conn, queue, callbacks=[self._on_message]):
+                        try:
+                            while self._running:
+                                try:
+                                    conn.drain_events(timeout=10)
+                                except timeout:
+                                    pass
+                        except Exception as e:
+                            logger.debug('Error when draining message queue: {0}'.format(e))
+            except IOError as e:
+                logger.info('Disconnected from MQ Server. Reconnecting in {0} seconds.'.format(
+                    self._reconnect_timeout))
+                sleep(self._reconnect_timeout)
+
+
+class Propaganda(object):
+    def __init__(self, exchange_name, broker_url=None, key_prefix=''):
+        if not broker_url:
+            if settings.PROPAGANDA_BROKER_URL:
+                broker_url = settings.PROPAGANDA_BROKER_URL
+            else:
+                raise Exception("You have to define PROPAGANDA_BROKER_URL variable in your django settings")
+
+        self._connection = Connection(broker_url)
+        self._exchange = Exchange(exchange_name, channel=self._connection)
+        self._key_prefix = key_prefix
+        self._payload = {}
+        self._subscriptions = {}
+
+    @property
+    def connection(self):
+        return self._connection
+
+    @property
+    def exchange(self):
+        return self._exchange
+
+    @property
+    def key_prefix(self):
+        return self._key_prefix
+
+    @property
+    def payload(self):
+        return self._payload
+
+    def subscribe(self, binding_key, reconnect_timeout=10, on_exception=None):
+        if binding_key in self._subscriptions and not self._subscriptions[binding_key].is_alive():
+            self._subscriptions[binding_key].start()
+        elif binding_key not in self._subscriptions:
+            self._subscriptions[binding_key] = Subscription(self._connection, self._exchange, binding_key,
+                                                            key_prefix=self._key_prefix,
+                                                            reconnect_timeout=reconnect_timeout,
+                                                            on_exception=on_exception)
+
+        return self._subscriptions[binding_key]
+
+    def unsubscribe(self, binding_key):
+        if binding_key in self._subscriptions:
+            self._subscriptions[binding_key].stop()
+
+    def _publish(self, routing_key, payload, **kwargs):
+        payload.update(self.payload)
+        kwargs['exchange'] = self._exchange
+        kwargs['routing_key'] = '{0}{1}'.format(self._key_prefix, routing_key)
+
+        with producers[self._connection].acquire(block=True) as producer:
+            publish = self._connection.ensure(producer, producer.publish, max_retries=3)
+            try:
+                publish(payload, **kwargs)
+            except OSError as e:
+                logger.error("Could not publish '{0}': {1}".format(kwargs['routing_key'], e))
+            else:
+                logger.debug("Published '{0}'".format(kwargs['routing_key']))
+
+    def publish(self, routing_key, payload=None, block=True, **kwargs):
+        if payload is None:
+            payload = {}
+        elif not isinstance(payload, dict):
+            logger.error('payload parameter must be a dictionary')
+            raise TypeError("payload parameter must be a dictionary")
+
+        if block:
+            self._publish(routing_key, payload, **kwargs)
+        else:
+            t = Thread(target=self._publish, args=(routing_key, payload), kwargs=kwargs)
+            t.start()
